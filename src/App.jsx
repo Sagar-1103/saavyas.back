170cdfb0a7dd530b37968c99b3a02271189d0d58
thisisbipin <bipinmjadav087@gmail.com>
2023-02-25 22:23:27 +0530

    added template

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/home/Home";
+import Register from "./components/register/Register";
+import Error404 from "./components/Errors/Error404";
+import Competitions from "./components/competitions/Competitions";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Events from "./components/Events/Events";
 
 function App() {
-    return <div className="App">hi</div>;
+    return (
+        <div className="App">
+            <Navbar />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="register" element={<Register />} />
+                    <Route path="competitions" element={<Competitions />} />
+                    <Route path="*" element={<Error404 />} />
+                    <Route path="events" element={<Events />} />
+                </Routes>
+            </BrowserRouter>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
