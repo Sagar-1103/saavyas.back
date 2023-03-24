aef8d92b667c94fe235d3e6a81a3e390b44b1677
Coding Club NIT Goa <123163039+codingclubnitg@users.noreply.github.com>
2023-03-24 18:21:19 +0530

    changed to BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/home/Home";
 import Register from "./components/register/Register";
@@ -10,7 +10,7 @@ import Events from "./components/Events/Events";
 
 function App() {
     return (
-        <HashRouter>
+        <BrowserRouter>
             <div className="App font-poppins flex flex-col min-h-screen">
                 <div className="flex-grow">
                     <Navbar />
@@ -24,7 +24,7 @@ function App() {
                 </div>
                 <Footer />
             </div>
-        </HashRouter>
+        </BrowserRouter>
     );
 }
 
