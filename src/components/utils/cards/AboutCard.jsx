0867ffc944f0b1cfbb39621c3baca783d5951e43
AbhiraajV <abhiraajverma@gmail.com>
2023-03-28 18:07:46 +0530

    about card image

--- src/components/utils/cards/AboutCard.jsx
+++ src/components/utils/cards/AboutCard.jsx
@@ -3,11 +3,9 @@ import React from 'react'
 function AboutCard() {
   return (
     
-        <div className="flex md:flex-col rounded-lg md:justify-start md:items-start md:px-2 justify-center items-center gap-3 bg-[#17294d] w-[97%] min-h-[150px] relative p-1 min-h-3">
+        <div style={{background: "linear-gradient(180deg, rgba(29, 48, 86, 0) 23.23%, #1D3056 100%)"}} className="flex md:flex-col rounded-lg md:justify-start md:items-start md:px-2 justify-center items-center gap-3 bg-[#17294d] w-[97%] min-h-[150px] relative p-1 min-h-3">
             
-            <div className="bg-[#111e38] p-2 md:w-[250px] md:h-[250px] w-[40%] h-[150px] rounded-md flex justify-center items-center">
-                <img src="https://i.dummyjson.com/data/products/1/1.jpg" alt="img" className='max-h-[150px] md:max-h-[250px]'/>
-            </div>
+            <div style={{ backgroundImage: "url(https://i.dummyjson.com/data/products/1/1.jpg)" }} className="bg-contain bg-center bg-no-repeat bg-[rgba(29, 48, 86, 0)] p-2 md:w-[250px] rounded-[50%] md:h-[250px] w-[150px] h-[150px] flex justify-center items-center"/>
 
             <div className="text-left w-[60%] h-[100%]  rounded-md flex gap-1 flex-col justify-center md:items-left md:justify-start">
                 <span className='text-left font-semibold md:text-lg text-lg font-lostfish'>
