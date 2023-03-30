921f5cda753a3bda91fdf8b04c6306204321e83a
thisisbipin1 <bipinmjadav087@outlook.com>
2023-03-31 01:01:05 +0530

    updated homepage messages

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -14,15 +14,19 @@ const Home = () => {
                 <div className="font-semibold justify-center items-center text-4xl font-lostfish">
                     About Us
                 </div>
-                <div className="w-[75%] justify-center items-center text-center text-md xl:w-[1280px] mx-auto">
-                    It is a long established fact that a reader will be distracted by the readable
-                    content of a page when looking at its layout. The point of using Lorem Ipsum is
-                    that it has a more-or-less normal distribution of letters, as opposed to using
-                    'Content here, content here', making it look like readable English. Many desktop
-                    publishing packages and web page editors now use Lorem Ipsum as their default
-                    model text, and a search for 'lorem ipsum' will uncover many web sites still in
-                    their infancy. Various versions have evolved over the years, sometimes by
-                    accident, sometimes on purpose (injected humour and the like).
+                <div className="w-[75%] justify-between items-center text-center text-md xl:w-[1280px] mx-auto">
+                    NIT Goa is one of the Premier Institutes of National Importance in India,
+                    currently operating at a transit campus at Ponda, while waiting for our
+                    Permanent Campus at Cuncolim to be ready. <br /> <br />
+                    It is no small feat that we have secured a NIRF rank of 88 in a transit campus,
+                    this shows the true potential of NIT Goa for growth and expansion. NIT Goa has
+                    organised various events and activities for its students and is now bringing
+                    SAAVYAS 2023 after a break of 3 years. <br /> <br /> SAAVYAS is NIT Goa's very
+                    own Techno-Cultural Fest, it comprises of a beautiful balance of various events
+                    catering to the very diverse audience of Goa. Students from all over Goa and
+                    beyond come together to participate in the event. <br /> <br />
+                    This year we wish to host the event on a much higher scale and make the fest a
+                    grand spectacle and a memorable event.
                 </div>
             </div>
 
