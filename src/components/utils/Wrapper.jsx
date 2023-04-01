b1ff7dd2f83810f1dfa117e7df5ff63232f8874a
Deep Shetye <deepshetye1@gmail.com>
2023-04-01 12:17:50 +0530

    FIX REQUIRED: Hamburger image not visible

--- src/components/utils/Wrapper.jsx
+++ src/components/utils/Wrapper.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 function Wrapper({ children }) {
-  return (
-    <div
-      style={{ backgroundImage: "url(images/bg.png)" }}
-      className="bg-contain bg-center-top bg-no-repeat w-full mx-auto bg-[#0F1B33] text-white gap-[6rem] md:gap-[10rem] flex flex-col justify-center items-center"
-    >
-      {children}
-    </div>
-  );
+    return (
+        <div className="w-full overflow-x-hidden text-white gap-[6rem] md:gap-[10rem] flex flex-col justify-center items-center">
+            <div
+                style={{
+                    backgroundImage: "url(images/bg.png)",
+                    backgroundPosition: "top center",
+                }}
+                className="h-[90vh] top-0 absolute w-screen w-d bg-no-repeat bg-[length:auto_100%] lg:bg-[length:100%_auto]"
+            />
+            {children}
+        </div>
+    );
 }
 
 export default Wrapper;
