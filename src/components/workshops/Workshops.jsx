1b021d1c0902bff123488b4c593f0156315f6f85
thisisbipin <bipinmjadav087@outlook.com>
2023-04-02 22:29:40 +0530

    updated workshops

--- src/components/workshops/Workshops.jsx
+++ src/components/workshops/Workshops.jsx
@@ -8,7 +8,7 @@ const Workshops = () => {
     let [workshops, setWorkshops] = useState(null);
 
     async function LoadData() {
-        let x = await GetDetailsOfEndPoint("workshops/");
+        let x = await GetDetailsOfEndPoint("events/workshop/");
         setWorkshops(x);
     }
 
@@ -18,18 +18,21 @@ const Workshops = () => {
 
     return (
         <Wrapper>
-            <div className="w-full flex justify-center text-center gap-[1.75rem] pt-40 h-80 pb-40 font-lostfish">
-                <ClearCard className="cursor-pointer">Technical</ClearCard>
-                <ClearCard className="cursor-pointer">Cultural</ClearCard>
-            </div>
+            <div className="w-full flex justify-center text-center gap-[1.75rem] pt-40 h-80 pb-40 font-lostfish"></div>
             <div className="p-3 w-full flex justify-center items-center flex-col gap-8">
                 <div className="font-semibold justify-center items-center md:text-4xl text-2xl font-lostfish">
                     Workshops
                 </div>
-                <div className="flex w-full md:w-[73%] md:flex-row md:overflow-y-hidden md:overflow-x-auto md:max-h-max items-center flex-col gap-2 max-h-[500px] overflow-y-scroll">
-                    {workshops &&
-                        Object.keys(workshops).map((key) => <EventCard event={workshops[key]} />)}
-                </div>
+                {workshops ? (
+                    <div className="flex w-full md:w-[73%] md:flex-row md:overflow-y-hidden md:overflow-x-auto md:max-h-max items-center flex-col gap-2 max-h-[500px] overflow-y-scroll">
+                        {workshops &&
+                            Object.keys(workshops).map((key) => (
+                                <EventCard event={workshops[key]} eventType={"workshop"} />
+                            ))}
+                    </div>
+                ) : (
+                    <h1 className="p-[10rem] text-3xl">We will be updating our Workshops Soon!</h1>
+                )}
             </div>
         </Wrapper>
     );
