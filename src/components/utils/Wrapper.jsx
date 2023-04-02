a105f7257260e6977beeb86f5c2ad833c5ccdb28
thisisbipin <bipinmjadav087@outlook.com>
2023-04-03 01:03:12 +0530

    fixed footer coming under image

--- src/components/utils/Wrapper.jsx
+++ src/components/utils/Wrapper.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 function Wrapper({ children }) {
     return (
-        <section className="bg-contain bg-top bg-no-repeat w-full mx-auto bg-[#0F1B33] z-[0] text-white gap-[6rem] md:gap-[10rem] flex flex-col justify-center items-center">
+        <section className="bg-contain bg-top bg-no-repeat w-full mx-auto z-[0] text-white gap-[6rem] md:gap-[10rem] flex flex-col justify-center items-center">
             <img
                 src="./images/bg.png"
-                className="absolute top-0 left-0 w-full min-h-1/2 object-cover z-[-1]"
+                style={{ zIndex: "-100000" }}
+                className="absolute top-0 left-0 w-full min-h-1/2 object-cover z-[-10]"
                 alt=""
             />
             {children}
