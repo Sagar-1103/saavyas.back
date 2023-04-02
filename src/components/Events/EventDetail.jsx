510e9881691a2cff8eeb5fc6d76450a5f6ade35d
thisisbipin <bipinmjadav087@outlook.com>
2023-04-02 11:29:21 +0530

    support dynamic contents

--- src/components/Events/EventDetail.jsx
+++ src/components/Events/EventDetail.jsx
@@ -47,7 +47,6 @@ const EventDetail = () => {
     const navigate = useNavigate();
     const width = useWindowWidth();
     const [event, setEvent] = React.useState(null);
-    const [sponsors, setSponsors] = React.useState(null);
 
     const [registerBtnRef, registerBtnIsHovered] = useHover();
 
@@ -64,20 +63,6 @@ const EventDetail = () => {
             }
 
             setEvent(events[category][eventId]);
-            setSponsors([
-                {
-                    name: "Google",
-                    image: "/images/sponsors/google.png",
-                },
-                {
-                    name: "Microsoft",
-                    image: "/images/sponsors/microsoft.png",
-                },
-                {
-                    name: "Amazon",
-                    image: "/images/sponsors/amazon.png",
-                },
-            ]);
 
             console.log(event);
         })();
@@ -98,7 +83,7 @@ const EventDetail = () => {
                 <div className="h-[32rem] w-full bg-[#D9D9D9] bg-opacity-[0.2] rounded-md flex flex-col items-center justify-end">
                     <div className=" w-full flex flex-col md:flex-row md:justify-center md:items-end">
                         <div className="font-poppins w-full md:flex-1  min-h-[12rem] md:flex md:flex-col md:justify-center md:gap-3 md:min-h-fit md:m-10 ">
-                            <h1 className="font-lostfish text-4xl text-center md:text-left w-full">
+                            <h1 className="font-stuart-little strong text-4xl text-center md:text-left w-full">
                                 {event && event.title}
                             </h1>
                             <p className="text-center md:text-left text-xl ">
@@ -138,7 +123,7 @@ const EventDetail = () => {
                         <h1 className="font-poppins capitalize text-3xl font-bold text-left w-full">
                             {event && event.title}
                         </h1>
-                        <p>{event && event.desc}</p>
+                        <p innerText="123">{event && event.desc}</p>
                         <p className="font-poppins font-bold text-left w-full text-lg">
                             Event Date:{" "}
                             <span className="font-normal">
@@ -151,7 +136,7 @@ const EventDetail = () => {
                             </span>
                         </p>
                         <p className="font-poppins font-bold text-left w-full text-lg">
-                            Event Location: NIT GOA
+                            Event Location: {event && event.vid}
                         </p>
                         <p className="font-poppins font-bold text-left w-full text-lg">
                             Registration Deadline:{" "}
@@ -165,58 +150,68 @@ const EventDetail = () => {
                             </span>
                         </p>
                     </div>
+                    {event && event.doc && (
+                        <a
+                            target="_blank"
+                            href={event.doc}
+                            className="cursor-pointer w-[80%] my-6  md:w-[17rem]  mx-auto py-1 flex font-lostfish text-xl justify-center items-center bg-[#A9CCEC] bg-opacity-[0.2] backdrop-blur-sm shadow-md rounded-md mt-5 md:m-10"
+                        >
+                            More Details &nbsp;
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                />
+                            </svg>
+                        </a>
+                    )}
                 </div>
 
                 {/* image carousel section  */}
-                <div className="w-full md:w-[760px] mx-auto h-[32rem] md:mt-[5rem]">
-                    <Carousel slideInterval={5000}>
-                        <img
-                            src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-                            alt="..."
-                        />
-                        <img
-                            src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-                            alt="..."
-                        />
-                        <img
-                            src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-                            alt="..."
-                        />
-                        <img
-                            src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-                            alt="..."
-                        />
-                        <img
-                            src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-                            alt="..."
-                        />
-                    </Carousel>
-                </div>
+                {event && event.event_images && (
+                    <div className="w-full md:w-[760px] mx-auto h-[32rem] md:mt-[5rem]">
+                        <Carousel slideInterval={5000}>
+                            {event.event_images.map((each) => (
+                                <img src={each} alt="..." />
+                            ))}
+                        </Carousel>
+                    </div>
+                )}
 
                 {/* sponsors section  */}
-                <div className="w-full mt-[3rem] md:mt-[5rem]">
-                    <h3 className="font-lostfish text-3xl underline underline-offset-[5px] text-center ">
-                        Event Sponsors
-                    </h3>
-                    <div className="flex flex-row flex-wrap justify-center gap-10 md:gap-20 mt-10 w-[80%] mx-auto">
-                        {sponsors &&
-                            sponsors.map((sponsor, index) => (
-                                <div
-                                    key={index}
-                                    className="flex flex-col items-center justify-center"
-                                >
-                                    <img
-                                        src={sponsor.image}
-                                        alt={sponsor.name}
-                                        className="w-[7rem] md:w-[10rem] h-[7rem] md:h-[10rem] rounded-full"
-                                    />
-                                    <p className="font-poppins text-xl font-normal mt-1">
-                                        {sponsor.name}
-                                    </p>
-                                </div>
-                            ))}
+                {event && event.sponsors && (
+                    <div className="w-full mt-[3rem] md:mt-[5rem]">
+                        <h3 className="font-lostfish text-3xl underline underline-offset-[5px] text-center ">
+                            Event Sponsors
+                        </h3>
+                        <div className="flex flex-row flex-wrap justify-center gap-10 md:gap-20 mt-10 w-[80%] mx-auto">
+                            {event.sponsors &&
+                                event.sponsors.map((sponsor, index) => (
+                                    <div
+                                        key={index}
+                                        className="flex flex-col items-center justify-center"
+                                    >
+                                        <img
+                                            src={sponsor.image}
+                                            alt={sponsor.name}
+                                            className="w-[7rem] md:w-[10rem] h-[7rem] md:h-[10rem] rounded-full"
+                                        />
+                                        <p className="font-poppins text-xl font-normal mt-1">
+                                            {sponsor.name}
+                                        </p>
+                                    </div>
+                                ))}
+                        </div>
                     </div>
-                </div>
+                )}
 
                 {/* event rules section */}
                 <div className="w-full mt-[3rem] md:mt-[5rem] ">
@@ -227,7 +222,7 @@ const EventDetail = () => {
                         {event &&
                             event.rules.map((rule, index) => (
                                 <li key={index} className="font-poppins text-xl font-normal mt-1">
-                                    {rule.length > 100 ? rule.slice(0, 100) + "..." : rule}
+                                    {rule}
                                 </li>
                             ))}
                     </ul>
@@ -238,35 +233,25 @@ const EventDetail = () => {
                     <h3 className="font-lostfish text-3xl underline underline-offset-[5px] text-center ">
                         Event Organizers
                     </h3>
-
                     <div className="w-[80%] mx-auto flex flex-col md:flex-row md:mt-[4rem] gap-4 mt-8">
-                        <div className="flex flex-col min-h-[7rem] w-full bg-gradient-to-t from-[#1D3056] to-transparent rounded-md py-5">
-                            <div className="flex flex-col flex-1">
-                                <h3 className="capitalize font-poppins text-xl font-bold tracking-wide text-center">
-                                    ABHINAV UPADHYAY
-                                </h3>
-                                <p className="text-center">(Coordinator)</p>
-                            </div>
+                        {event &&
+                            event.contact &&
+                            event.contact.map((each) => (
+                                <div className="flex flex-col min-h-[7rem] w-full bg-gradient-to-t from-[#1D3056] to-transparent rounded-md py-5">
+                                    <div className="flex flex-col flex-1">
+                                        <h3 className="capitalize font-poppins text-xl font-bold tracking-wide text-center">
+                                            {each.name}
+                                        </h3>
+                                        <p className="text-center">({each.designation})</p>
+                                    </div>
 
-                            <div>
-                                <p className="text-center text-lg">Mob. No.: xxxxxxxxxx</p>
-                                <p className="text-center text-lg">Email Id.: abc@gmail.com</p>
-                            </div>
-                        </div>
-
-                        <div className="flex flex-col min-h-[7rem] w-full bg-gradient-to-t from-[#1D3056] to-transparent rounded-md py-5">
-                            <div className="flex flex-col flex-1">
-                                <h3 className="capitalize font-poppins text-xl font-bold tracking-wide text-center">
-                                    ABHINAV UPADHYAY
-                                </h3>
-                                <p className="text-center">(Coordinator)</p>
-                            </div>
-
-                            <div>
-                                <p className="text-center text-lg">Mob. No.: xxxxxxxxxx</p>
-                                <p className="text-center text-lg">Email Id.: abc@gmail.com</p>
-                            </div>
-                        </div>
+                                    <div>
+                                        <p className="text-center text-lg">
+                                            Mob. No: <a href={"tel:" + each.phone}>{each.phone}</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            ))}
                     </div>
                 </div>
             </div>
