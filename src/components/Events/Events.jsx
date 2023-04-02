a748518ef119b9fdf686165747a6cdd6c53bcf8e
thisisbipin <bipinmjadav087@outlook.com>
2023-04-03 00:42:14 +0530

    items-stretch

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -19,14 +19,13 @@ const Events = () => {
     return (
         <Wrapper>
             <div className="w-full flex justify-center text-center gap-[1.75rem] md:pt-[20rem] pt-[15rem] h-80 pb-40 font-lostfish">
-                <ClearCard className="cursor-pointer">Technical</ClearCard>
-                <ClearCard className="cursor-pointer">Cultural</ClearCard>
+                <p className="text-6xl">Events</p>
             </div>
             <div className="p-3 w-full flex justify-center items-center flex-col gap-8">
                 <div className="font-semibold justify-center items-center md:text-4xl text-2xl font-lostfish">
                     Technical Events
                 </div>
-                <div className="flex max-w-[1280px] w-full md:w-[73%] md:flex-row md:overflow-y-hidden md:overflow-x-auto md:max-h-max items-center flex-col gap-2 max-h-[500px] overflow-y-scroll">
+                <div className="flex max-w-[1280px] w-full md:w-[73%] md:flex-row md:overflow-y-hidden md:overflow-x-auto md:max-h-max items-center flex-col gap-2 max-h-[500px] overflow-y-scroll  items-stretch">
                     {events &&
                         Object.keys(events.technical).map((key) => (
                             <EventCard eventType="technical" event={events.technical[key]} />
@@ -38,7 +37,7 @@ const Events = () => {
                 <div className="font-semibold justify-center items-center md:text-4xl text-2xl font-lostfish">
                     Cultural Events
                 </div>
-                <div className="flex w-full max-w-[1280px] md:w-[73%] md:flex-row md:overflow-y-hidden md:overflow-x-auto md:max-h-max items-center flex-col gap-2 max-h-[500px] overflow-y-scroll">
+                <div className="flex w-full max-w-[1280px] md:w-[73%] md:flex-row md:overflow-y-hidden md:overflow-x-auto md:max-h-max items-center flex-col gap-2 max-h-[500px] overflow-y-scroll items-stretch">
                     {events &&
                         Object.keys(events.cultural).map((key) => (
                             <EventCard eventType="cultural" event={events.cultural[key]} />
