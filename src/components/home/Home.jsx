6a474e0eaab9871a2e3daee3709ce490e176bde2
thisisbipin <bipinmjadav087@outlook.com>
2023-04-06 01:06:00 +0530

    updated homepage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import pg from "./stuff.json";
 import Wrapper from "../utils/Wrapper";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    let analytics = pg.analytics;
     return (
         <Wrapper>
-            <div className="flex h-[80vh] justify-center items-center text-left text-5xl md:text-6xl p-[20px] h-80 pb-40 font-lostfish pt-[300px] md:pt-[400px] text-left md:w-full">
+            <div className="flex h-[80vh] justify-center items-center text-left text-5xl md:text-6xl p-[20px] h-80 font-lostfish pt-[200px] md:pt-[400px] text-left md:w-full">
                 Sea Shore Soiree
             </div>
 
@@ -22,34 +21,35 @@ const Home = () => {
                             className="rounded-full"
                         />
                     </div>
-                    <div className="w-3/4">
-                        NIT Goa is one of the Premier Institutes of National Importance in India,
-                        currently operating at a transit campus at Ponda, while waiting for our
-                        Permanent Campus at Cuncolim to be ready. It is no small feat that we have
-                        secured a NIRF rank of 88 in a transit campus, this shows the true potential
-                        of NIT Goa for growth and expansion. NIT Goa has organised various events
-                        and activities for its students and is now bringing SAAVYAS 2023 after a
-                        break of 3 years. <br /> <br /> SAAVYAS is NIT Goa's very own
-                        Techno-Cultural Fest, it comprises of a beautiful balance of various events
-                        catering to the very diverse audience of Goa. Students from all over Goa and
-                        beyond come together to participate in the event. This year we wish to host
-                        the event on a much higher scale and make the fest a grand spectacle and a
-                        memorable event.
+                    <div className="w-3/4 md:text-justify text-left">
+                        SAAVYAS is NIT Goa's very own Techno - Cultural Fest, it comprises of a
+                        beautiful balance of various events catering to the very diverse audience of
+                        Goa. Students from all over Goa and beyond come together to participate in
+                        the event. This year we wish to host the event on a much higher scale and
+                        make the fest a grand spectacle and a memorable event.
                     </div>
                 </div>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 min-[3000px]:grid-cols-2 gap-6 pb-48">
-                {analytics.map((point) => (
-                    <div className="md:w-[25vw] lg:h-[30vh] w-[60vw] h-[40vh] bg-[#5A7D9A40] flex flex-col justify-center items-center gap-[2rem]">
+            <div className="flex justify-center align-center flex-wrap">
+                <Link to="/events">
+                    <div className="transition-all m-[10px] md:w-[25vw] lg:h-[30vh] w-[60vw] h-[40vh] bg-[#5A7D9A40] hover:bg-[#c1e1fc40] flex flex-col justify-center items-center gap-[2rem]">
                         <span className="font-semibold text-3xl min-[1200px]:text-7xl text-white">
-                            {point.value}
+                            Events
                         </span>
                         <span className="font-semibold text-3xl min-[3000px]:text-7xl text-white">
-                            {point.title}
+                            40+
                         </span>
                     </div>
-                ))}
+                </Link>
+                <div className="m-[10px] md:w-[25vw] lg:h-[30vh] w-[60vw] h-[40vh] bg-[#5A7D9A40] flex flex-col justify-center items-center gap-[2rem]">
+                    <span className="font-semibold text-3xl min-[1200px]:text-7xl text-white">
+                        Colleges
+                    </span>
+                    <span className="font-semibold text-3xl min-[3000px]:text-7xl text-white">
+                        20+
+                    </span>
+                </div>
             </div>
 
             {false && (
