59db8c734811a3e3a2aab7b51b1f4bc8f1c53a34
thisisbipin <bipinmjadav087@outlook.com>
2023-04-05 14:52:31 +0530

    added lottie player animation

--- src/components/utils/Wrapper.jsx
+++ src/components/utils/Wrapper.jsx
@@ -4,12 +4,25 @@ import "./Wrapper.css";
 function Wrapper({ children, text = "Sea Shore Soiree" }) {
     return (
         <section className="bg-contain bg-top bg-no-repeat w-full mx-auto z-[0] text-white gap-[6rem] md:gap-[10rem] flex flex-col justify-center items-center">
-            <img
+            {/* <img
                 src="./images/bg.png"
                 style={{ zIndex: "-100000" }}
                 className="absolute top-0 left-0 w-full min-h-1/2 object-cover z-[-10]"
                 alt=""
-            />
+            /> */}
+            <div
+                style={{ minWidth: "100%", minHeight: "100%" }}
+                className="absolute top-0 left-0 w-full min-h-1/2 object-cover z-[-10] overflow-hidden"
+            >
+                <lottie-player
+                    src="/animation.json"
+                    background="transparent"
+                    speed="0.5"
+                    loop
+                    style={{ minWidth: "110%", minHeight: "100%" }}
+                    autoplay
+                ></lottie-player>
+            </div>
             {children}
         </section>
     );
