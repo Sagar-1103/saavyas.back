dfdfb8bf699a2ef0e2788b787182e3d8f83d89e2
thisisbipin <bipinmjadav087@outlook.com>
2023-04-18 10:02:03 +0530

    added dev team

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ import Events from "./components/Events/Events";
 import { AuthContextProvider } from "./components/context/AuthContext";
 import EventRegister from "./components/register/EventRegister";
 import Contact from "./components/Contact/Contact";
-import About from "./components/About/About";
+import DevTeam from "./components/DevTeam/DevTeam";
 import Notification from "./components/Notification/Notification";
 import EventDetail from "./components/Events/EventDetail";
 import Wrapper from "./components/utils/Wrapper";
 import { useState } from "react";
+import CodingClub from "./components/CodingClub/CodingClub";
 
 function App() {
     let [text, settext] = useState("Sea Shore Soiree");
@@ -37,11 +38,12 @@ function App() {
                                 path="events/:category/:eventId"
                                 element={<EventDetail settext={settext} />}
                             />
-                            <Route path="about" element={<About />} />
+                            <Route path="codingclubnitg" element={<DevTeam settext={settext} />} />
                         </Routes>
                     </AuthContextProvider>
                 </div>
                 <Footer />
+                <CodingClub />
             </div>
         </BrowserRouter>
     );
